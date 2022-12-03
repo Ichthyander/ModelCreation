4cdc03f4a432c5916e269140b901fc5ea88b02fe
Ichtyander <di_ignatov@mail.ru>
2022-12-03 18:42:56 +0300

    Добавьте файлы проекта.

---
+++ ModelCreation.cs
@@ -0,0 +1,75 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelCreation
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    class ModelCreation : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document document = commandData.Application.ActiveUIDocument.Document;
+
+            List<Wall> walls = CreateWalls(document);
+
+            return Result.Succeeded;
+        }
+
+        public List<Level> GetLevels(Document document)
+        {
+            return new FilteredElementCollector(document)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+        }
+
+        public List<Wall> CreateWalls(Document document)
+        {
+            var levelsList = GetLevels(document);
+
+            var level1 = levelsList
+                .Where(x => x.Name.Equals("Уровень 1"))
+                .FirstOrDefault();
+
+            var level2 = levelsList
+                .Where(x => x.Name.Equals("Уровень 2"))
+                .FirstOrDefault();
+
+            double width = UnitUtils.ConvertToInternalUnits(10000, DisplayUnitType.DUT_MILLIMETERS);
+            double depth = UnitUtils.ConvertToInternalUnits(5000, DisplayUnitType.DUT_MILLIMETERS);
+
+            double dx = width / 2;
+            double dy = depth / 2;
+
+            List<XYZ> points = new List<XYZ>();
+            points.Add(new XYZ(-dx, -dy, 0));
+            points.Add(new XYZ(dx, -dy, 0));
+            points.Add(new XYZ(dx, dy, 0));
+            points.Add(new XYZ(-dx, dy, 0));
+            points.Add(new XYZ(-dx, -dy, 0));
+
+            List<Wall> walls = new List<Wall>();
+
+            Transaction transaction = new Transaction(document, "Create walls");
+            transaction.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Line line = Line.CreateBound(points[i], points[i + 1]);
+                Wall wall = Wall.Create(document, line, level1.Id, false);
+                walls.Add(wall);
+                wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
+            }
+
+            transaction.Commit();
+
+            return walls;
+        }
+    }
+}
