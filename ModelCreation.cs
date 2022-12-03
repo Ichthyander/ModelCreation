e8d089e2cfab0e04f2bac361ba68ff1bf90e8d2f
Ichtyander <di_ignatov@mail.ru>
2022-12-03 22:43:00 +0300

    AddRoof method was implemented.

--- ModelCreation.cs
+++ ModelCreation.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
 
 namespace ModelCreation
 {
@@ -30,9 +31,106 @@ namespace ModelCreation
 
             AddDoor(document, baseLevel, walls[0]);
             AddWindows(document, baseLevel, walls.GetRange(1, 3));
+            AddRoof(document, level2, walls);
 
             return Result.Succeeded;
         }
+
+        private void AddRoof(Document document, Level roofLevel, List<Wall> walls)
+        {
+            RoofType roofType = new FilteredElementCollector(document)
+                .OfClass(typeof(RoofType))
+                .OfType<RoofType>()
+                .Where(x => x.Name.Equals("Типовой - 400мм"))
+                .Where(x => x.FamilyName.Equals("Базовая крыша"))
+                .FirstOrDefault();
+
+            double wallWidth = walls[0].Width;
+            double dt = wallWidth / 2;
+
+            //List<XYZ> points = new List<XYZ>();
+            //points.Add(new XYZ(-dt, -dt, 0));
+            //points.Add(new XYZ(dt, -dt, 0));
+            //points.Add(new XYZ(dt, dt, 0));
+            //points.Add(new XYZ(-dt, dt, 0));
+            //points.Add(new XYZ(-dt, -dt, 0));
+
+            Application application = document.Application;
+            CurveArray curveArray = application.Create.NewCurveArray();
+            LocationCurve curve = walls[0].Location as LocationCurve;
+            XYZ p1 = new XYZ(curve.Curve.GetEndPoint(0).X, curve.Curve.GetEndPoint(0).Y, walls[0].get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsDouble());
+            
+            XYZ p2 = new XYZ((curve.Curve.GetEndPoint(0).X + curve.Curve.GetEndPoint(1).X)/2.0, 
+                (curve.Curve.GetEndPoint(0).Y + curve.Curve.GetEndPoint(1).Y) / 2.0, 
+                walls[0].get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsDouble() + UnitUtils.ConvertToInternalUnits(1500, DisplayUnitType.DUT_MILLIMETERS));
+
+            XYZ p3 = new XYZ(curve.Curve.GetEndPoint(1).X, curve.Curve.GetEndPoint(1).Y, walls[0].get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsDouble());
+
+            curveArray.Append(Line.CreateBound(p1, p2));
+            curveArray.Append(Line.CreateBound(p2, p3));
+
+            View view = new FilteredElementCollector(document)
+                .OfClass(typeof(View))
+                .Cast<View>()
+                .Where(x => x.Name.Equals("Южный"))
+                .FirstOrDefault();
+
+
+            Transaction transaction = new Transaction(document, "Create ExtrusionRoof");
+            transaction.Start();
+
+            ReferencePlane plane = document.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(1, 0, 0), new XYZ(0, 0, 1), view);
+
+            document.Create.NewExtrusionRoof(curveArray, plane, roofLevel, roofType, 
+                -1*(walls[1].Location as LocationCurve).Curve.ApproximateLength / 2 - dt, (walls[1].Location as LocationCurve).Curve.ApproximateLength / 2 + dt);
+
+            transaction.Commit();
+        }
+
+        //private void AddRoof(Document document, Level roofLevel, List<Wall> walls)
+        //{
+        //    RoofType roofType = new FilteredElementCollector(document)
+        //        .OfClass(typeof(RoofType))
+        //        .OfType<RoofType>()
+        //        .Where(x => x.Name.Equals("Типовой - 400мм"))
+        //        .Where(x => x.FamilyName.Equals("Базовая крыша"))
+        //        .FirstOrDefault();
+
+        //    double wallWidth = walls[0].Width;
+        //    double dt = wallWidth / 2;
+
+        //    List<XYZ> points = new List<XYZ>();
+        //    points.Add(new XYZ(-dt, -dt, 0));
+        //    points.Add(new XYZ(dt, -dt, 0));
+        //    points.Add(new XYZ(dt, dt, 0));
+        //    points.Add(new XYZ(-dt, dt, 0));
+        //    points.Add(new XYZ(-dt, -dt, 0));
+
+        //    Application application = document.Application;
+        //    CurveArray footprint = application.Create.NewCurveArray();
+
+        //    for (int i = 0; i < 4; i++)
+        //    {
+        //        LocationCurve curve = walls[i].Location as LocationCurve;
+        //        XYZ p1 = curve.Curve.GetEndPoint(0);
+        //        XYZ p2 = curve.Curve.GetEndPoint(1);
+        //        Line line = Line.CreateBound(p1 + points[i], p2 + points[i + 1]);
+        //        footprint.Append(line);
+        //    }
+
+        //    Transaction transaction = new Transaction(document, "Create Roof");
+        //    transaction.Start();
+        //    ModelCurveArray footprintToModelCurveMapping = new ModelCurveArray();
+        //    FootPrintRoof footprintRoof = document.Create.NewFootPrintRoof(footprint, roofLevel, roofType, out footprintToModelCurveMapping);
+
+        //    foreach(ModelCurve modelCurve in footprintToModelCurveMapping)
+        //    {
+        //        footprintRoof.set_DefinesSlope(modelCurve, true);
+        //        footprintRoof.set_SlopeAngle(modelCurve, 0.5);
+        //    }
+
+        //    transaction.Commit();
+        //}
 
         private void AddWindows(Document document, Level baseLevel, List<Wall> walls)
         {
